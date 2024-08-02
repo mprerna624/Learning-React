b0ac78a2a773b2125fa004343288b2aba5ad8039
Prerna Mittal <mprerna624@gmail.com>
2024-08-02 11:33:29 +0530

    Added updateTodo & toggleChecked functionality

--- TodoReduxToolkit/src/App.jsx
+++ TodoReduxToolkit/src/App.jsx
@@ -1,14 +1,27 @@
+import { useSelector } from 'react-redux'
 import './App.css'
 import TodoForm from './Components/TodoForm'
 import TodoItems from './Components/TodoItems'
 
 function App() {
 
+  const todos = useSelector( (state) => state.todos )
+
   return (
     <div className="w-full text-center mx-auto px-auto">
       <h1>Todo App using Redux Toolkit</h1>
       <TodoForm />
-      <TodoItems />
+
+      <div className="flex flex-wrap gap-y-3 mt-6">
+        {
+          todos.map((eachTodo) => (
+            <div key={eachTodo.id} className='w-full'>
+              <TodoItems todo={eachTodo} />
+            </div>
+          ))
+        }
+      </div>
+      
     </div>
   )
 }
--- TodoReduxToolkit/src/features/todo/todoSlice.js
+++ TodoReduxToolkit/src/features/todo/todoSlice.js
@@ -3,7 +3,8 @@ import { createSlice, nanoid } from "@reduxjs/toolkit";
 const initialState = {
     todos: [{
         id: nanoid(),
-        task: "Hello World"
+        task: "Hello World",
+        completed: false
     }]
 }
 
@@ -14,15 +15,25 @@ export const todoSlice = createSlice({
         addTodo: (state, action) => {
             const newTodo = {
                 id: nanoid(),
-                task: action.payload
+                task: action.payload,
+                completed: false
             }
             state.todos.push(newTodo)
         },
+
         deleteTodo: (state, action) => {
             state.todos = state.todos.filter( (eachTodo) => eachTodo.id !== action.payload);
+        },
+
+        updateTodo: (state, action) => {
+            state.todos = state.todos.map( (eachTodo) => eachTodo.id === action.payload.id ? {...eachTodo, task : action.payload.task} : eachTodo )
+        },
+        
+        toggleChecked: (state, action) => {
+            state.todos = state.todos.map( (eachTodo) => eachTodo.id === action.payload ? {...eachTodo, completed: !eachTodo.completed} : eachTodo )
         }
     }
 })
 
-export const {addTodo, deleteTodo} = todoSlice.actions;
+export const {addTodo, deleteTodo, updateTodo, toggleChecked} = todoSlice.actions;
 export default todoSlice.reducer;
